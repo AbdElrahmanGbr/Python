e8cc4e9922065f263ba954a28b60543712f56a2c
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-04-23 00:33:51 +0200

    working with database

--- Day3/Employement-System/EmployementSystem/EmployementSystem/settings.py
+++ Day3/Employement-System/EmployementSystem/EmployementSystem/settings.py
@@ -74,10 +74,17 @@ WSGI_APPLICATION = 'EmployementSystem.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'abdelrahman',
+'USER': 'root',
+'PASSWORD': "",
+'HOST': "localhost",
+'PORT': "3306",
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
+}
 }
 
 
